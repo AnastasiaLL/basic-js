250754a7354adfadf04b45fe5b88c402ade4e0df
AnastasiaLL <lukyanchik.nastya@bk.ru>
2022-04-30 19:41:21 +0300

    feat: add recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
+ * Привет НАСТЯ!!!!
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
  * 
@@ -12,12 +13,90 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
+//  class DepthCalculator {
+
+//   calculateDepth(arr) {
+//     if (!Array.isArray(arr)) {
+//       return 0
+//     }
+//     let maxDepth = 1;
+//     let depth =1;
+//     rec(arr)
+//     function rec(el){
+
+//         el.forEach(elem => {
+//             if (Array.isArray(elem) ){
+//                 depth = depth + 1;
+//                 rec(elem)
+//             }
+//         });
+//         if (depth > maxDepth){
+//             maxDepth = depth;
+//         }
+//         depth = depth -1;
+
+//     }
+
+//       return maxDepth
+
+//     }
+// }
+
+//  class DepthCalculator {
+
+//   constructor(arr) {
+//     if (!Array.isArray(arr)) {
+//       return 0
+//     }
+//     // if (!Array.isArray(array)) throw new Error("Параметр не является маcсивом.");
+//     this.arr = arr;
+//     this.depth = 1;
+//     this.maxDepth = 1;
+//   }
+
+//   calculateDepth(arr = this.arr, depth = 1) {
+
+
+//     arr.forEach(elem => {
+//       if (Array.isArray(elem) ){
+//         this.depth = this.depth + 1;
+//         this.calculateDepth(elem)
+//       }
+//     });
+
+//     if (this.depth > this.maxDepth){
+//       this.maxDepth = this.depth;
+//     }
+//     this.depth = this.depth -1;
+
+
+
+//     return this.maxDepth
+
+//   }
+// }
+
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.depth = 1;
+  }
+  calculateDepth(array, depth = 1) {
+    if (!Array.isArray(array)) {
+      return 0
+    }
+    if (depth == 1) {
+      this.depth = 1
+    }
+    if (depth > this.depth) this.depth = depth;
+    for (let e of array) {
+      if (Array.isArray(e)) {
+        this.calculateDepth(e, depth + 1);
+      }
+    }
+    return this.depth;
   }
 }
+
 
 module.exports = {
   DepthCalculator
